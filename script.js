cbfdf28f7aa23647a65e959bc1d4dc14a7b98cf6
Moses <mosesireri2004@gmail.com>
2026-07-12 09:09:11 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,395 @@
+const experiences = [
+    {
+        duration: "2022 - Present",
+        role: "Frontend Developer",
+        corporation: "Google",
+        description: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Nisi ad nam alias quo! Obcaecati, voluptas."
+    },
+
+        {
+        duration: "2021 -2022",
+        role: "Full Stack Developer",
+        corporation: "YouTube",
+        description: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Nisi ad nam alias quo! Obcaecati, voluptas."
+    },
+
+        {
+        duration: "2020 - 2021",
+        role: "Backend Developer",
+        corporation: "Apple Inc.",
+        description: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Nisi ad nam alias quo! Obcaecati, voluptas."
+    },
+
+        {
+        duration: "2019 - 2020",
+        role: "Frontend Developer Intern",
+        corporation: "Amazon.com Inc.",
+        description: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Nisi ad nam alias quo! Obcaecati, voluptas."
+    },
+
+        {
+        duration: "2018 - 2019",
+        role: "UI / UX Designer",
+        corporation: "Microsoft Corp.",
+        description: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Nisi ad nam alias quo! Obcaecati, voluptas."
+    },
+
+        {
+        duration: "2027 - 2018",
+        role: "Junior Developer",
+        corporation: "Chevron Corp.",
+        description: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Nisi ad nam alias quo! Obcaecati, voluptas."
+    },
+];
+
+const skills = [
+    {
+        Skill: "HTML5",
+        logo: "bxl-html5"
+    },
+    {
+        Skill: "CSS3",
+         logo: "bxl-css3"
+    },
+        {
+        Skill: "Javascript",
+         logo: "bxl-javascript"
+    },
+    {
+        Skill: "React.js",
+         logo: "bxl-react"
+    },
+    {
+        Skill: "Tailwind.css",
+         logo: "bxl-tailwind-css"
+    },
+    {
+        Skill: "Bootstrap",
+         logo: "bxl-bootstrap"
+    },
+    {
+        Skill: "Angular",
+         logo: "bxl-angular"
+    },
+    {
+        Skill: "Vue.js",
+         logo: "bxl-vuejs"
+    },
+    {
+        Skill: "Node.js",
+         logo: "bxl-nodejs"
+    },
+    {
+        Skill: "Java",
+         logo: "bxl-java"
+    },
+    {
+        Skill: "PHP",
+         logo: "bxl-php"
+    },
+    {
+        Skill: "Python",
+         logo: "bxl-python"
+    },
+    {
+        Skill: "Django",
+         logo: "bxl-django"
+    },
+    {
+        Skill: "Flask",
+         logo: "bxl-flask"
+    },
+    {
+        Skill: "MongoDB",
+         logo: "bxl-mongodb"
+    },
+    {
+        Skill: "PostgreSQL",
+         logo: "bxl-postgresql"
+    },
+        {
+        Skill: "Sketch",
+         logo: "bxl-sketch"
+    },
+    {
+        Skill: "Figma",
+         logo: "bxl-figma"
+    },
+];
+
+const Education = [
+    {
+        year: "2024",
+        course: "Programming Course",
+        mode: "Online Course",
+        details: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsam dolorem et optio a in?"
+    },
+
+        {
+        year: "2022 - 2023",
+        course: "Web Development Bootcamp",
+        mode: "Online Course Platform",
+        details: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsam dolorem et optio a in?"
+    },
+
+        {
+        year: "2020",
+        course: "Certified Web Development",
+        mode: "Online Course",
+        details: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsam dolorem et optio a in?"
+    },
+
+        {
+        year: "2017 - 2018",
+        course: "Doctor of Engineering",
+        mode: "Codehal University",
+        details: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsam dolorem et optio a in?"
+    },
+        {
+        year: "2015 - 2017",
+        course: "Master of Computer Science",
+        mode: "Codehal University",
+        details: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsam dolorem et optio a in?"
+    },
+        {
+        year: "2012 - 2017",
+        course: "Bachelor of Data Science",
+        mode: "Codehal University",
+        details: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsam dolorem et optio a in?"
+    },
+];
+
+const projects = [
+   {
+      project_image: "./project_1.jpeg",
+      project_title: "Frontend Project",
+      technologies: "HTML5, CSS3, Javascript",
+      info: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Ullam eligendi porro numquam."
+   },
+      {
+      project_image: "./project_2.jpeg",
+      project_title: "Full Stack Project",
+      technologies: "Next.js, Tailwind.css, Node.js",
+      info: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Ullam eligendi porro numquam."
+   },
+      {
+      project_image: "./project_3.png",
+      project_title: "Frontend Project",
+      technologies: "React.js, Tailwind.css",
+      info: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Ullam eligendi porro numquam."
+   },
+      {
+      project_image: "./project_4.jpeg",
+      project_title: "Frontend Project",
+      technologies: "HTML5, Bootstrap, Javascript",
+      info: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Ullam eligendi porro numquam."
+   },
+      {
+      project_image: "./project_5.jpeg",
+      project_title: "Frontend Project",
+      technologies: "Next.js, Tailwind.css",
+      info: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Ullam eligendi porro numquam."
+   },
+      {
+      project_image: "./project_6.jpg",
+      project_title: "Full Stack Project",
+      technologies: "Next.js, Tailwind.css, Node.js",
+      info: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Ullam eligendi porro numquam."
+   },
+];
+
+const services = [
+    {
+        service_icon: "bx-code-alt",
+        Service: "Web Development",
+        service_info: "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Eos doloremque quis sunt incidunt nemo harum?",
+    },
+        {
+        service_icon: "bx-palette",
+        Service: "Graphic Design",
+        service_info: "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Eos doloremque quis sunt incidunt nemo harum?",
+    },
+        {
+        service_icon: "bxs-paint",
+        Service: "UI/UX Design",
+        service_info: "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Eos doloremque quis sunt incidunt nemo harum?",
+    },
+        {
+        service_icon: "bx-signal-5",
+        Service: "SEO",
+        service_info: "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Eos doloremque quis sunt incidunt nemo harum?",
+    },
+        {
+        service_icon: "bx-slideshow",
+        Service: "Video Editing",
+        service_info: "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Eos doloremque quis sunt incidunt nemo harum?",
+    },
+        {
+        service_icon: "bx-camera",
+        Service: "Photography",
+        service_info: "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Eos doloremque quis sunt incidunt nemo harum?",
+    },
+]
+
+const resumeLists = document.querySelectorAll(".resume-list");
+const resumeBoxes = document.querySelectorAll(".resume-box");
+const portfoiloLists = document.querySelectorAll(".portfolio-list");
+const portfolioBoxes = document.querySelectorAll(".portfolio-box");
+const sections = document.querySelectorAll(".section");
+const navLinks = document.querySelectorAll(".nav-list li");
+
+navLinks.forEach((navLink, index) => {
+  navLink.addEventListener("click", () => {
+    document.querySelector(".nav-list li.active").classList.remove("active");
+    navLink.classList.add("active");
+
+document.querySelector(".box").style.transform = `rotateY(${index * -90}deg)`;
+
+document.querySelector(".section.active").classList.remove("active");
+sections[index].classList.add("active");
+
+const array = Array.from(sections);
+const arraySecs = array.slice(1, -1);
+arraySecs.forEach(arraySec => {
+    if (arraySec.classList.contains("active")){
+        sections[4].classList.add("action-contact");
+    };
+});
+if (sections[0].classList.contains("active")){
+    sections[4].classList.remove("action-contact");
+};
+  });
+});
+
+
+resumeLists.forEach((resumeList, index) => {
+  resumeList.addEventListener("click", () => {
+    document.querySelector(".resume-list.active").classList.remove("active");
+    resumeList.classList.add("active");
+
+    document.querySelector(".resume-box.active").classList.remove("active");
+    resumeBoxes[index].classList.add("active");
+  });
+});
+
+portfoiloLists.forEach((portfolioList, index) => {
+  portfolioList.addEventListener("click", () => {
+    document.querySelector(".portfolio-list.active").classList.remove("active");
+    portfolioList.classList.add("active");
+
+      document.querySelector(".portfolio-box.active").classList.remove("active");
+    portfolioBoxes[index].classList.add("active");
+  });
+});
+
+(function generateExperienceResume (){
+    let experienceItem = "";
+
+    experiences.forEach((experience) => {
+      experienceItem += `
+          <div class="tab-item resume-item">
+                <h4>${experience.duration}</h4>
+                <h4>${experience.role}</h4>
+                <h4>${experience.corporation}</h4>
+                <p>${experience.description}</p>
+              </div>
+      `;
+
+      document.querySelector(".tab-grid.experience").innerHTML = experienceItem;
+    });
+
+})();
+
+(function generateSkillsesume () {
+    let  skillItem = "";
+
+    skills.forEach((skill) => {
+        skillItem += `
+          <div class="tab-item resume-item">
+                <i class="bx ${skill.logo}"></i>
+                <p>${skill.Skill}</p>
+              </div>
+        `;
+    });
+
+    document.querySelector(".tab-grid.skills").innerHTML = skillItem;
+})();
+
+(function generateEducationResume (){
+    let educationItem = "";
+
+    Education.forEach((education) => {
+       educationItem += `
+               <div class="tab-item resume-item">
+                <h4>${education.year}</h4>
+                <h4>${education.course}</h4>
+                <h4>${education.mode}</h4>
+                <p>${education.details}</p>
+              </div>
+       `;
+    });
+
+    document.querySelector(".tab-grid.education").innerHTML = educationItem;
+})();
+
+(function generateProjectContent (){
+    let projectContent = "";
+
+    projects.forEach((project) => {
+        projectContent += `
+         <div class="tab-item portfolio-item">
+                <div class="work-img">
+                  <img src="./images/${project.project_image}" alt=""/>
+                </div>
+
+                <div class="work-info">
+                  <h4>${project.project_title}</h4>
+                  <p>${project.info}</p>
+                  <div class="technologies">
+                    <p>${project.technologies}</p>
+                  </div>
+
+                  <div class="portfolio-icon work-icon">
+                    <a href="#">
+                      <i class="bx bx-arrow-back"></i>
+                      <span class="tooltip">Preview</span></a
+                    >
+
+                    <a href="#">
+                      <i class="bx bxl-github"></i>
+                      <span class="tooltip">Github Repository</span></a
+                    >
+                  </div>
+                </div>
+              </div>
+        `;
+
+    });
+
+    document.querySelector(".portfolio-box.work").innerHTML = projectContent;
+
+})();
+
+(function generateServicesContent (){
+    let serviceContent = "";
+
+    services.forEach((service) => {
+       serviceContent += `
+                  <div class="tab-item portfolio-item">
+                <div class="portfolio-icon service-icon">
+                    <i class="bx ${service.service_icon} icon"></i>
+                    <a href="#"><i class="bx bx-arrow-back"></i></a>
+                </div>
+                <h4>${service.Service}</h4>
+                <p>${service.service_info}</p>
+              </div>
+       `;
+    });
+
+
+    document.querySelector(".portfolio-box.service").innerHTML = serviceContent;
+})();
+
+setTimeout(() => {
+    sections[4].classList.remove("active");
+}, 1500);
